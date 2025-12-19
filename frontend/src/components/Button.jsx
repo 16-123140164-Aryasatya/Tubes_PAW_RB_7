10411cba1aed96b78deadb141535c1ea46e57243
Ivan Nandira <ivannandira@gmail.com>
2025-12-19 19:13:37 +0700

    Update Button.jsx
    import React di file Button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,8 @@
-export default function Button({ children, variant = "default", ...props }) {
+import React from "react";
+
+export default React.memo(function Button({ children, variant = "default", ...props }) {
   let cls = "btn";
   if (variant === "primary") cls += " btn-primary";
   if (variant === "danger") cls += " btn-danger";
   return <button className={cls} {...props}>{children}</button>;
-}
+});
